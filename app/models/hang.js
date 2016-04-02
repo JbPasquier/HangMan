5b39f173ac76dd351e7d76d3ff035966a92bdf9a
Jean-Baptiste <contact@jbpasquier.eu>
2016-04-02 19:19:37 +0200

    Setting backend (miss return founded letters)

--- app/models/hang.js
+++ app/models/hang.js
@@ -1,40 +1,98 @@
-// MODEL TODO
+// MODEL
 var mongoose = require('mongoose');
-
+var randomWord = require('random-word');
 
 var hangSchema = new mongoose.Schema({
-    description: String
+    word: String,
+    found: String,
+    err: Number,
+    chars: []
 });
 
 var Hang = {
 
     model: mongoose.model('Hang', hangSchema),
 
-    create: function(req, res) {
+    readme: function(req, res) {
+        res.send(
+            'Welcome to the Hangman' + "\n" +
+            '# METHODS' + "\n" + 'GET /hangman' + "\t" + 'This readme' + "\n" +
+            'GET /hangman/new' + "\t" + 'Launch a new game, return your gameid' + "\n" +
+            'GET /hangman/new/n' + "\t" + 'Launch a new game, with a word of n letters, return your gameid' + "\n" +
+            'GET /hangman/gameid' + "\t" + 'Show your status' + "\n" +
+            'POST /hangman/gameid' + "\t" + 'Try a {guess:"word"}' + "\n" +
+            'PUT /hangman/gameid' + "\t" + 'Try to {guess:"a"} letter'
+        );
+    },
+
+    init: function(req, res) {
+        var word = '';
+        if (!req.params.num) {
+            word = randomWord();
+        } else {
+            while (word.length != req.params.num) {
+                word = randomWord();
+            }
+        }
+        var found = word.replace(/[a-z]{1}/g, '_');
         Hang.model.create({
-            description: req.body.description
+            word: word,
+            found: found,
+            err: 0,
+            chars: []
         }, function() {
-            res.sendStatus(200);
+            Hang.model.findOne({
+                word: word,
+                found: found,
+                err: 0,
+                chars: []
+            }, function(err, data) {
+                res.send(data._id);
+            });
         });
     },
 
-    findAll: function(req, res) {
-        Hang.model.find(function(err, data) {
-            res.send(data);
+    stat: function(req, res) {
+        Hang.model.findOne({_id:req.params.id},function(err, data) {
+            res.send('Now : '+data.found+"\n"+'You did '+data.err+'/10 err.'+"\n"+'You played : '+data.chars.join(', '));
         });
     },
 
-    update: function(req, res) {
-        Hang.model.findByIdAndUpdate(req.params.id, {
-            description: req.body.description
-        }, function() {
-            res.sendStatus(200);
+    sendChar: function(req, res) {
+        Hang.model.findOne({_id:req.params.id},function(err, data) {
+            data.word == data.found && res.send('You already found that word.'+"\n"+'Word was '+data.word);
+            data.err >= 11 && res.send('You have too much errors on this hangman.'+"\n"+'Word was '+data.word);
+            req.body.guess.length !== 1 && res.send('Please send only one char');
+            data.chars.indexOf(req.body.guess) !== -1 && res.send('You already send this char');
+            regex = new RegExp(req.body.guess,'g');
+            data.chars.push(req.body.guess);
+            if(regex.test(data.word)) {
+                data.found = data.found.replace(regex,req.body.guess);console.log(data.found);
+                Hang.model.findByIdAndUpdate(req.params.id,data,function() {
+                    res.send('Found '+data.word.match(regex)+"\n"+'Now : '+data.found);
+                });
+            } else {
+                data.err++;
+                Hang.model.findByIdAndUpdate(req.params.id,data,function() {
+                    res.send('Don\'t found '+data.word.match(regex)+"\n"+'Now : '+data.found);
+                });
+            }
         });
     },
 
-    delete: function(req, res) {
-        Hang.model.findByIdAndRemove(req.params.id, function() {
-            res.sendStatus(200);
+    guess: function(req, res) {
+        Hang.model.findOne({_id:req.params.id},function(err, data) {
+            if(data.word == req.body.guess) {
+                data.found = data.word;
+                Hang.model.findByIdAndUpdate(req.params.id,data,function() {
+                    res.send('Well done !'+"\n"+'The word was '+data.word+"\n"+'You did it with '+data.err+' err.');
+                });
+            } else {
+                data.err++;
+                Hang.model.findByIdAndUpdate(req.params.id,data,function() {
+                    res.send('Nope. Try again'+"\n"+'Now : '+data.found);
+                });
+            }
         });
     }
 };
--- app/routes/hang.js
+++ app/routes/hang.js
@@ -2,9 +2,11 @@
 var Hang = require('../models/hang.js');
 module.exports = function(app) {
 
-    app.get('/hang', Hang.findAll);
-    app.post('/hang', Hang.create);
-    app.put('/hang/:id', Hang.update);
-    app.delete('/hang/:id', Hang.delete);
+    app.get('/hangman', Hang.readme);
+    app.get('/hangman/new', Hang.init);
+    app.get('/hangman/new/:num', Hang.init);
+    app.get('/hangman/:id', Hang.stat);
+    app.post('/hangman/:id', Hang.guess);
+    app.put('/hangman/:id', Hang.sendChar);
 
 };
